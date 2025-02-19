27a14fdaf979f4add38eb642b817f16ab026a492
Astraalito <nicolas.asri@gmail.com>
2025-02-19 13:48:01 +0100

    fixed earth rotation on Mobile

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
@@ -13,6 +13,21 @@ const Earth = () => {
 };
 
 const EarthCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+    
+  useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 500px)");
+        setIsMobile(mediaQuery.matches);
+        const handleMediaQueryChange = (event) => {
+          setIsMobile(event.matches);
+        };
+        mediaQuery.addEventListener("change", handleMediaQueryChange);
+        return () => {
+          mediaQuery.removeEventListener("change", handleMediaQueryChange);
+        };
+  }, []);
+
   return (
     <Canvas
       shadows
@@ -30,6 +45,8 @@ const EarthCanvas = () => {
         <OrbitControls
           autoRotate
           enableZoom={false}
+          enablePan={false}
+          enableRotate={!isMobile}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
