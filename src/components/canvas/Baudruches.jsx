6cc60ba00689f7d3af0f2ca05e219f2af816170c
Astraalito <nicolas.asri@gmail.com>
2025-03-31 21:49:38 +0200

    added different colors for balloons

--- src/components/canvas/Baudruches.jsx
+++ src/components/canvas/Baudruches.jsx
@@ -1,14 +1,34 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 
-const BaudrucheModel = ({ position }) => {
+
+const colorsBaudruches = [
+    [1, 0, 0], // Rouge
+    [0, 1, 0], // Vert
+    [1, 1, 0], // Jaune
+    [1, 0, 1], // Magenta
+    [0, 1, 1], // Cyan
+  ];
+
+const BaudrucheModel = ({ position, color }) => {
     const { scene } = useGLTF("./balloon/baudruche.gltf");
     const meshRef = useRef();
 
+    useEffect(() => {
+        if (meshRef.current) {
+            meshRef.current.traverse((child) => {
+                if (child.isMesh) {
+                    child.material = child.material.clone();
+                    child.material.color.setRGB(...color);
+                }
+            });
+        }
+    }, [color]);
+
     useFrame(() => {
         if (meshRef.current) {
-            meshRef.current.position.y += 0.05; // Ajustez la vitesse de déplacement ici
+            meshRef.current.position.y += 0.05; 
         }
     });
 
@@ -18,7 +38,7 @@ const BaudrucheModel = ({ position }) => {
         position={position} 
         scale={0.7} 
     />;
-}
+};
 
 const Baudruches = ({ numberBaudruche = 20, rayon = 8, yPosition = -7.3, offset = 3 }) => {
     
@@ -41,14 +61,18 @@ const Baudruches = ({ numberBaudruche = 20, rayon = 8, yPosition = -7.3, offset 
     };
 
 
-   const positions = generateRandomPositions(numberBaudruche, rayon, yPosition);
+    const positions = generateRandomPositions(numberBaudruche, rayon, yPosition);
+
+    const choseRandomColor = () => {
+        return colorsBaudruches[Math.floor(Math.random() * colorsBaudruches.length)];
+    }
 
 
     return (
         <>
-            {positions.map((pos, index) => (
-                <BaudrucheModel key={index} position={pos} />
-            ))}
+            {positions.map((pos, index) => {
+                return <BaudrucheModel key={index} position={pos} color={choseRandomColor()} />
+            })}
         </>
     )
 }
